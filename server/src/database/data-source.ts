0aef6c1737369fd33244527b3e920e186619ef4c
Gabriel Gigante <gabrielgigante29@gmail.com>
2022-04-04 08:12:25 -0300

    feat: setup docker image and docker-compose

--- server/src/database/data-source.ts
+++ server/src/database/data-source.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const appDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'user',
-  password: 'pass',
-  database: 'app-dws',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: Number(process.env.DATABASE_PORT) || 5432,
+  username: process.env.DATABASE_USER || 'user',
+  password: process.env.DATABASE_PASS || 'pass',
+  database: process.env.DATABASE_NAME || 'app-dws',
   synchronize: true,
   logging: false,
   entities: ['src/entities/*.ts'],
