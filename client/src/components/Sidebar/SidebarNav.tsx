a45f96575a87a847361a001174b764e48ab0d1d6
Gabriel Gigante <gabrielgigante29@gmail.com>
2022-04-05 13:56:39 -0300

    feat: creating sidebar component

---
+++ client/src/components/Sidebar/SidebarNav.tsx
@@ -0,0 +1,15 @@
+import { Stack } from '@chakra-ui/react'
+import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from 'react-icons/ri'
+
+import { NavSection } from './NavSection'
+import { NavLink } from './NavLink'
+
+export function SidebarNav() {
+  return (
+    <Stack spacing="12" align="flex-start">
+      <NavSection title="GERAL">
+        <NavLink icon={RiContactsLine} href="/cotacoes">Cotações</NavLink>
+      </NavSection>
+    </Stack>
+  )
+}
---
+++ client/src/models/Cotacao.ts
@@ -0,0 +1,7 @@
+export interface Cotacao {
+  id: number;
+  productName: string;
+  productPrice: number;
+  paymentType: string;
+  portionPrice: number;
+}
